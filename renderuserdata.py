de3f86089287b64f971b44fae3e9cfed4de43389
Kiran Munnanoor <munnanoor.java@gmail.com>
2021-02-28 02:53:30 -0600

    working on rederuserdata.py

--- renderuserdata.py
+++ renderuserdata.py
@@ -4,6 +4,49 @@ import dash_html_components as html
 import plotly.express as px
 import pandas as pd
 from pathlib import Path
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:admin@localhost:5432/example'
+db = SQLAlchemy(app)
+
+class SleepData(db.Model):
+    __tablename__ = 'sleep_data'
+    User_ID
+    cycle_id
+    Date
+    Day_of_Week
+    RHR	
+    HRV
+    Recovery
+    Sleep_Score
+    Sleep_Onset
+    Wake_Onset
+    Hours_in_Bed
+    Hours_of_Sleep
+    Sleep_Need
+    Sleep_Efficiency_Percentage
+    Wake_Periods
+    Sleep_Disturbances
+    Latency_Minimum	
+    Cycles
+    REM_Sleep_hours
+    Deep_Sleep hours
+    Light_Sleep_hours
+    Awake_hours	
+    Missing_Data_hours
+    Sleep_Debt_hours
+    Sleep_Consistency
+    Respiratory_Rate
+    Last_Nap_End_Time
+    Nap_Time_in_Bed
+    Total Cycle Nap Time hours	Nap Disturbances	Nap REM	Nap SWS	Nap Light	Nap Wake	Nap Missing Data	Total Cycle Sleep Time hours	timezone_offset	REM Percentage	Deep Sleep Percentage	Restorative Sleep hours	Restorative Sleep Percentage
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(), nullable=False)
+db.create_all()
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
