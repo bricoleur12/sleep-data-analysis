ba573496eb677925ff66d9d49f424228137a0f3c
Kiran Munnanoor <munnanoor.java@gmail.com>
2021-02-28 18:51:15 -0600

    Version after discussion with Keerthana.

--- app.py
+++ app.py
@@ -1,20 +1,37 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+from flask import request
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:admin@localhost:5432/sleepdata'
 db = SQLAlchemy(app)
 
+# This model or class corresponds to the userdatafiles table in the sleepdata db in postgres
 class UserDataFiles(db.Model):
     __tablename__ = 'userdatafiles'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(), nullable=False)
+    fileLocation = db.Column(db.String(), nullable=False)
 
 def __repr__(self):
     return f' <UserDataFiles {self.id} {self.name}>'
 
 db.create_all()
 
+def shutdown_server():
+    func = request.environ.get('werkzeug.server.shutdown')
+    if func is None:
+        raise RuntimeError('Not running with the Werkzeug Server')
+    func()
+    
+@app.route('/shutdown', methods=['GET'])
+def shutdown():
+    shutdown_server()
+    return 'Server shutting down...' 
+
+
+
 @app.route('/')
 def index():
     return render_template('index.html', data= UserDataFiles.query.all() )  
+
--- renderuserdata.py
+++ renderuserdata.py
@@ -15,35 +15,6 @@ db = SQLAlchemy(app)
 
 class SleepData(db.Model):
     __tablename__ = 'sleep_data'
-    User_ID
-    cycle_id
-    Date
-    Day_of_Week
-    RHR	
-    HRV
-    Recovery
-    Sleep_Score
-    Sleep_Onset
-    Wake_Onset
-    Hours_in_Bed
-    Hours_of_Sleep
-    Sleep_Need
-    Sleep_Efficiency_Percentage
-    Wake_Periods
-    Sleep_Disturbances
-    Latency_Minimum	
-    Cycles
-    REM_Sleep_hours
-    Deep_Sleep hours
-    Light_Sleep_hours
-    Awake_hours	
-    Missing_Data_hours
-    Sleep_Debt_hours
-    Sleep_Consistency
-    Respiratory_Rate
-    Last_Nap_End_Time
-    Nap_Time_in_Bed
-    Total Cycle Nap Time hours	Nap Disturbances	Nap REM	Nap SWS	Nap Light	Nap Wake	Nap Missing Data	Total Cycle Sleep Time hours	timezone_offset	REM Percentage	Deep Sleep Percentage	Restorative Sleep hours	Restorative Sleep Percentage
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(), nullable=False)
 db.create_all()
